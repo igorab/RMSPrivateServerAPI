5b88a414f0b691ffff439d0a9ffd469304963ebe
Balakirev Igor <igor.balakirev@mail.ru>
2025-07-11 22:16:18 +0300

    Controller

--- Controllers/RobotController.cs
+++ Controllers/RobotController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RMSPrivateServerAPI.DTOs;
+using RMSPrivateServerAPI.Entities;
+using RMSPrivateServerAPI.Interfaces;
 using RMSPrivateServerAPI.Models;
+using RMSPrivateServerAPI.Repositories;
 using RMSPrivateServerAPI.Services;
 
 namespace RMSPrivateServerAPI.Controllers;
@@ -10,12 +14,45 @@ namespace RMSPrivateServerAPI.Controllers;
 [Route("api/Robot/v1.0/robot")]
 public class RobotController : ControllerBase
 {
+    private readonly ILogger<RobotController> _logger;
+    private readonly IRobotRepository _robotRepository;
     private readonly RobotService _robotService;
+    private readonly IMapper _mapper;
 
-    public RobotController(RobotService robotService)
+    public RobotController(ILogger<RobotController> logger,
+                           IRobotRepository robotRepository,
+                           RobotService robotService,
+                           IMapper  mapper)
+                            
     {
+        _logger = logger;
+        _robotRepository = robotRepository;
         _robotService = robotService;
+        _mapper = mapper;
     }
+
+
+    /// <summary>
+    /// Get all the robots in the Database 
+    /// </summary>
+    /// <param name="returnDeletedRecords">If true, the method will return all the records</param>     
+    [HttpGet]
+    public async Task<IEnumerable<Robot>> GetAll([FromRoute] bool returnDeletedRecords = false)
+    {
+        return await _robotRepository.GetAll(returnDeletedRecords);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Robot>> Get(int id)
+    {
+        var robot = await _robotService.Get(id);
+        if (robot == null)
+        {
+            return NotFound();
+        }
+        return robot;
+    }
+
 
     [HttpGet("List")]
     public ActionResult GetRobots()
