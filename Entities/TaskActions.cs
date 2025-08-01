a84a5bdf0e648a51cbbe17f77f758c60ff599d80
Balakirev Igor <igor.balakirev@mail.ru>
2025-08-01 14:35:58 +0300

    Task, TaskActions

---
+++ Entities/TaskActions.cs
@@ -0,0 +1,42 @@
+﻿namespace RMSPrivateServerAPI.Entities;
+
+using System;
+
+/// <summary>
+/// Класс для работы с таблицей TaskActions
+/// </summary>
+public class TaskActions
+{
+    /// <summary>
+    /// Уникальный идентификатор действия
+    /// </summary>
+    public int Id { get; set; }
+    /// <summary>
+    /// Идентификатор связанной задачи
+    /// </summary>
+    public Guid TaskId { get; set; }
+    /// <summary>
+    /// Дата и время создания действия
+    /// </summary>
+    public DateTime CreatedAt { get; set; }
+    /// <summary>
+    /// Текущий статус действия
+    /// </summary>
+    public string Status { get; set; }
+    /// <summary>
+    /// Местоположение, где выполнено действие
+    /// </summary>
+    public string Location { get; set; }
+    /// <summary>
+    /// Тип действия
+    /// </summary>
+    public int ActionType { get; set; }
+    /// <summary>
+    /// Порядок действия
+    /// </summary>
+    public int ActionOrder { get; set; } 
+    /// <summary>
+    /// Навигационное свойство для связи с задачей (если необходимо)
+    /// </summary>
+    public virtual Task Task { get; set; } // Связь с классом Task
+}
---
+++ Entities/Tasks.cs
@@ -0,0 +1,36 @@
+﻿namespace RMSPrivateServerAPI.Entities;
+
+/// <summary>
+/// таблица, которая хранит информацию о задачах в приложении
+/// </summary>
+public class Tasks
+{
+    /// <summary>
+    /// Unique identifier for each task.
+    /// </summary>
+    public Guid TaskId { get; set; }
+    /// <summary>
+    /// Timestamp when the task was created.
+    /// </summary>
+    public DateTime CreatedAt { get; set; }
+    /// <summary>
+    /// Timestamp when the task was processed.
+    /// </summary>
+    public DateTime? ProcessedAt { get; set; }
+    /// <summary>
+    /// Current status of the task.
+    /// </summary>
+    public string Status { get; set; }
+    /// <summary>
+    /// Identifier for the store in WMS.
+    /// </summary>
+    public string StoreWmsId { get; set; }
+    /// <summary>
+    /// Identifier for the area in WMS.
+    /// </summary>
+    public string AreaWmsId { get; set; }
+    /// <summary>
+    /// Priority level of the task.
+    /// </summary>
+    public int Priority { get; set; }
+}
