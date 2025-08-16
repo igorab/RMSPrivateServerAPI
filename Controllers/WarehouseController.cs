ac79d729ab886bc1b2ecb923e6d1bd1fa29c2f30
Balakirev Igor <igor.balakirev@mail.ru>
2025-08-16 23:39:04 +0300

    warehouse

---
+++ Controllers/WarehouseController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RMSPrivateServerAPI.Data;
+using RMSPrivateServerAPI.StoreMapDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RMSPrivateServerAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WarehouseController : ControllerBase
+{
+    private readonly WarehouseDbContext _context;
+
+    public WarehouseController(WarehouseDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/warehouse/stores
+    [HttpGet("stores")]
+    public async Task<ActionResult<IEnumerable<Store>>> GetStores()
+    {
+        return await _context.Stores.ToListAsync();
+    }
+
+    // GET: api/warehouse/areas
+    [HttpGet("areas")]
+    public async Task<ActionResult<IEnumerable<Area>>> GetAreas()
+    {
+        return await _context.Areas.ToListAsync();
+    }
+
+    // GET: api/warehouse/zones
+    [HttpGet("zones")]
+    public async Task<ActionResult<IEnumerable<Zone>>> GetZones()
+    {
+        return await _context.Zones.ToListAsync();
+    }
+
+    // GET: api/warehouse/points
+    [HttpGet("points")]
+    public async Task<ActionResult<IEnumerable<Point>>> GetPoints()
+    {
+        return await _context.Points.ToListAsync();
+    }
+
+    // GET: api/warehouse/paths
+    [HttpGet("paths")]
+    public async Task<ActionResult<IEnumerable<StoreMapDto.Path>>> GetPaths()
+    {
+        return await _context.Paths.ToListAsync();
+    }
+
+    // GET: api/warehouse/pathElements
+    [HttpGet("pathElements")]
+    public async Task<ActionResult<IEnumerable<PathElement>>> GetPathElements()
+    {
+        return await _context.PathElements.ToListAsync();
+    }
+
+    // GET: api/warehouse/zoneTypes
+    [HttpGet("zoneTypes")]
+    public async Task<ActionResult<IEnumerable<ZoneType>>> GetZoneTypes()
+    {
+        return await _context.ZoneTypes.ToListAsync();
+    }
+
+    // GET: api/warehouse/pointTypes
+    [HttpGet("pointTypes")]
+    public async Task<ActionResult<IEnumerable<PointType>>> GetPointTypes()
+    {
+        return await _context.PointTypes.ToListAsync();
+    }
+
+    // GET: api/warehouse/pathElementTypes
+    [HttpGet("pathElementTypes")]
+    public async Task<ActionResult<IEnumerable<PathElementType>>> GetPathElementTypes()
+    {
+        return await _context.PathElementTypes.ToListAsync();
+    }
+}
---
+++ Data/WarehouseDbContext.cs
@@ -0,0 +1,43 @@
+﻿namespace RMSPrivateServerAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using RMSPrivateServerAPI.StoreMapDto;
+
+public class WarehouseDbContext : DbContext
+{
+    public WarehouseDbContext(DbContextOptions<WarehouseDbContext> options) : base(options)
+    {
+    }
+
+    // DbSet для таблицы Store
+    public DbSet<Store> Stores { get; set; }
+
+    // DbSet для таблицы Area
+    public DbSet<Area> Areas { get; set; }
+
+    // DbSet для таблицы Zone
+    public DbSet<Zone> Zones { get; set; }
+
+    // DbSet для таблицы Point
+    public DbSet<Point> Points { get; set; }
+
+    // DbSet для таблицы Path
+    public DbSet<Path> Paths { get; set; }
+
+    // DbSet для таблицы PathElement
+    public DbSet<PathElement> PathElements { get; set; }
+
+    // DbSet для таблицы ZoneType
+    public DbSet<ZoneType> ZoneTypes { get; set; }
+
+    // DbSet для таблицы PointType
+    public DbSet<PointType> PointTypes { get; set; }
+
+    // DbSet для таблицы PathElementType
+    public DbSet<PathElementType> PathElementTypes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Здесь можно настроить дополнительные параметры для сущностей, если это необходимо
+        base.OnModelCreating(modelBuilder);
+    }
+}
