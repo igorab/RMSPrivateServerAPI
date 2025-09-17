39add857322dd6744f636bdf9449e80a4ee0991e
Balakirev Igor <igor.balakirev@mail.ru>
2025-09-17 17:55:40 +0300

    app.Environment.IsDevelopment убрал

--- Program.cs
+++ Program.cs
@@ -103,11 +103,13 @@ public partial class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        //if (app.Environment.IsDevelopment())
+        //{
+        //    app.UseSwagger();
+        //    app.UseSwaggerUI();
+        //}
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
         app.UseHttpsRedirection();
 
